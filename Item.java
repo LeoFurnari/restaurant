3c02ade01ae9ffdd18ebc2bbd87085e12360cc9b
Andrew Basore <basor1aj@cmich.edu>
2016-03-30 00:27:12 -0400

    Item.java
    Class definition of an Item

---
+++ Item.java
@@ -0,0 +1,20 @@
+package application;
+/*
+ * Item is a generic type for menu items. We will make classes that extend from Item to make different types of items (Desserts/Entrees ect.)
+ */
+public class Item {
+	double price;
+	String name;
+	//ArrayList<Modifier>  modifierList; (NYI)
+	
+	
+	
+	
+	
+	
+	public String toString(){
+		//Account for modifiers(??)
+		return null;
+	}
+
+}
