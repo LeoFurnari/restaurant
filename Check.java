ef278d745172109a9b2e88729d0e0e18dd1717a2
Andrew Basore <basor1aj@cmich.edu>
2016-03-30 00:28:25 -0400

    Create Check.java

---
+++ Check.java
@@ -0,0 +1,43 @@
+package application;
+
+import java.util.ArrayList;
+/*
+ * Check class holds all information for guest checks and payment
+ */
+public class Check {
+	int checkNum; 
+	ArrayList<Item> itemList;
+	
+	
+	public String print(){         //This method prints the check as a reciept to the console (Or to a printer)
+		return null;
+	}
+	
+	private double calculateTotal(){
+		return 0;
+	}
+	/*
+	 * SQL code 
+	 * 
+	String returnedThing; //button return thing: item name
+	int orderNum; //button return thing: item order number
+	double price; //button return thing: item price
+	String insertString = "INSERT INTO Item VALUES " + returnedThing;
+	String selectString = "SELECT FROM Item VALUES " + orderNum;
+	*/
+	
+	/*
+	 * Payment methods to clear guest-debt to restaurant
+	 */
+	private void payCash(){
+		
+	}
+	
+	private void payCredit(){
+		/*
+		 * Implement credit gratuity?
+		 */
+		
+	}
+	
+}
